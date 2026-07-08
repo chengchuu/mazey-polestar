cc07ce5517233289bdb5a4da9d88ade164b00f73
Cheng <mazeyqian@gmail.com>
2026-07-08 15:24:56 +0800

    feat: move userscriptHeaders to a separate config file

---
+++ config/userscript.js
@@ -0,0 +1,20 @@
+const userscriptHeaders = {
+  webhook: `// ==UserScript==
+// @name         Telegram Webhook Monitor
+// @namespace    https://github.com/chengchuu/webpack-build-demo
+// @version      0.4.0
+// @description  Scan Telegram Web messages and send new readable messages to a webhook relay.
+// @match        https://web.telegram.org/*
+// @updateURL    https://raw.githubusercontent.com/chengchuu/webpack-build-demo/preview/lib/webhook.user.js
+// @downloadURL  https://raw.githubusercontent.com/chengchuu/webpack-build-demo/preview/lib/webhook.user.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
+// @connect      *
+// ==/UserScript==`,
+};
+
+module.exports = {
+  userscriptHeaders,
+};
--- webpack.config.lib.js
+++ webpack.config.lib.js
@@ -1,29 +1,13 @@
 const path = require("path");
 const webpack = require("webpack");
 const { genCustomConsole } = require("mazey");
+const { userscriptHeaders } = require("./config/userscript");
 
 const WebpackCon = genCustomConsole("WebpackCon:");
 const ENTRY = process.env.ENTRY;
 WebpackCon.log(`ENTRY ${ENTRY}`);
 const ENTRY_FILE = `./src/${ENTRY}.js` || "./src/index.js";
 WebpackCon.log(`ENTRY_PATH ${ENTRY_FILE}`);
-
-const userscriptHeaders = {
-  webhook: `// ==UserScript==
-// @name         Telegram Webhook Monitor
-// @namespace    https://github.com/chengchuu/webpack-build-demo
-// @version      0.4.0
-// @description  Scan Telegram Web messages and send new readable messages to a webhook relay.
-// @match        https://web.telegram.org/*
-// @updateURL    https://raw.githubusercontent.com/chengchuu/webpack-build-demo/preview/lib/webhook.user.js
-// @downloadURL  https://raw.githubusercontent.com/chengchuu/webpack-build-demo/preview/lib/webhook.user.js
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @grant        GM_xmlhttpRequest
-// @grant        GM_registerMenuCommand
-// @connect      *
-// ==/UserScript==`,
-};
 
 const plugins = [];
 
