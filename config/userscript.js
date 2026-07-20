366b4f734717b282d325329f39dfdddc69f7410a
Cheng <mazeyqian@gmail.com>
2026-07-20 21:20:24 +0800

    chore: update icon

--- config/userscript.js
+++ config/userscript.js
@@ -8,7 +8,7 @@ const userscriptHeaders = {
 // @namespace    https://github.com/chengchuu/webpack-build-demo
 // @version      ${userscriptVersion}
 // @description  Scan configured website messages and send new readable content to a webhook relay.
-// @icon         https://i.mazey.net/icon/fav/logo-dark-circle-32x32.png
+// @icon         https://i.mazey.net/icon/fav/logo-peace-circle-transparent-200x200.png
 // @match        https://web.telegram.org/*
 // @updateURL    https://raw.githubusercontent.com/chengchuu/webpack-build-demo/preview/lib/webhook.user.js
 // @downloadURL  https://raw.githubusercontent.com/chengchuu/webpack-build-demo/preview/lib/webhook.user.js
